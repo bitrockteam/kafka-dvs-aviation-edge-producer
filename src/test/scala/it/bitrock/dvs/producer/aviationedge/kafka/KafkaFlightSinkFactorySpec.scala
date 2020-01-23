30905fdd15dfc8210272d5e77593d45e64772cdd
Daniele Bonelli <daniele.bonelli@bitrock.it>
2020-01-23 15:23:45 +0100

    embedded kafka use free port on OS instead of fixed ones

--- src/test/scala/it/bitrock/dvs/producer/aviationedge/kafka/KafkaFlightSinkFactorySpec.scala
+++ src/test/scala/it/bitrock/dvs/producer/aviationedge/kafka/KafkaFlightSinkFactorySpec.scala
@@ -6,81 +6,57 @@ import akka.stream.scaladsl.Source
 import akka.testkit.TestKit
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig
 import it.bitrock.dvs.producer.aviationedge.TestValues
-import it.bitrock.dvs.producer.aviationedge.model.MessageJson
+import it.bitrock.dvs.producer.aviationedge.kafka.KafkaTypes.Flight.Value
 import it.bitrock.dvs.producer.aviationedge.kafka.KafkaTypes.{Key, Flight => KafkaTypesFlight}
+import it.bitrock.dvs.producer.aviationedge.model.MessageJson
 import it.bitrock.kafkacommons.serialization.ImplicitConversions._
-import it.bitrock.testcommons.{FixtureLoanerAnyResult, Suite}
+import it.bitrock.testcommons.Suite
+import net.manub.embeddedkafka.schemaregistry.EmbeddedKafka._
 import net.manub.embeddedkafka.schemaregistry._
-import org.apache.kafka.common.serialization.{Serde, Serdes}
-import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import org.apache.kafka.common.serialization.{Serde, Serdes, Serializer}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
 
 class KafkaFlightSinkFactorySpec
     extends TestKit(ActorSystem("KafkaFlightSinkFactorySpec"))
     with Suite
-    with WordSpecLike
+    with AnyWordSpecLike
     with BeforeAndAfterAll
-    with EmbeddedKafka
     with TestValues {
-  import KafkaFlightSinkFactorySpec._
 
   "sink method" should {
 
-    "convert a domain model to Kafka model and push it to a topic" in ResourceLoaner.withFixture {
-      case Resource(embeddedKafkaConfig, keySerde, factory) =>
-        implicit val embKafkaConfig: EmbeddedKafkaConfig = embeddedKafkaConfig
-        implicit val kSerde: Serde[Key]                  = keySerde
-        val result = withRunningKafka {
-          Source.single(FlightMessage).runWith(factory.sink)
-          consumeFirstKeyedMessageFrom[Key, KafkaTypesFlight.Value](factory.topic)
-        }
-        result shouldBe ((IcaoNumber, ExpectedFlightRaw))
-    }
+    "convert a domain model to Kafka model and push it to a topic" in {
+      val embeddedKafkaConfig: EmbeddedKafkaConfig = EmbeddedKafkaConfig(kafkaPort = 0, zooKeeperPort = 0, schemaRegistryPort = 0)
+      val outputTopic                              = "output_topic"
 
-  }
+      implicit val keySerde: Serde[Key] = Serdes.String
 
-  object ResourceLoaner extends FixtureLoanerAnyResult[Resource] {
-    override def withFixture(body: Resource => Any): Any = {
-      implicit val embeddedKafkaConfig: EmbeddedKafkaConfig = EmbeddedKafkaConfig()
+      val result = withRunningKafkaOnFoundPort(embeddedKafkaConfig) { implicit conf =>
+        implicit val valueSerializer: Serializer[Value] = specificAvroValueSerializer[KafkaTypesFlight.Value]
+        val producerSettings = ProducerSettings(system, keySerde.serializer, valueSerializer)
+          .withBootstrapServers(s"localhost:${conf.kafkaPort}")
+          .withProperty(
+            AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+            s"http://localhost:${conf.schemaRegistryPort}"
+          )
 
-      val outputTopic       = "output_topic"
-      val rsvpRawKeySerde   = Serdes.String
-      val rsvpRawSerializer = specificAvroValueSerializer[KafkaTypesFlight.Value]
-
-      val producerSettings = ProducerSettings(system, rsvpRawKeySerde.serializer, rsvpRawSerializer)
-        .withBootstrapServers(s"localhost:${embeddedKafkaConfig.kafkaPort}")
-        .withProperty(
-          AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
-          s"http://localhost:${embeddedKafkaConfig.schemaRegistryPort}"
+        val factory = new KafkaSinkFactory[MessageJson, Key, KafkaTypesFlight.Value](
+          outputTopic,
+          producerSettings
         )
 
-      val factory = new KafkaSinkFactory[MessageJson, Key, KafkaTypesFlight.Value](
-        outputTopic,
-        producerSettings
-      )
-
-      body(
-        Resource(
-          embeddedKafkaConfig,
-          rsvpRawKeySerde,
-          factory
-        )
-      )
+        Source.single(FlightMessage).runWith(factory.sink)
+        consumeFirstKeyedMessageFrom[Key, KafkaTypesFlight.Value](factory.topic)
+      }
+      result shouldBe ((IcaoNumber, ExpectedFlightRaw))
     }
+
   }
 
   override def afterAll(): Unit = {
     shutdown()
     super.afterAll()
   }
-
-}
-
-object KafkaFlightSinkFactorySpec {
-
-  final case class Resource(
-      embeddedKafkaConfig: EmbeddedKafkaConfig,
-      keySerde: Serde[Key],
-      factory: KafkaSinkFactory[MessageJson, Key, KafkaTypesFlight.Value]
-  )
 
 }
