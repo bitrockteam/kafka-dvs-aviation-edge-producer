7a88afd738a884bc0a2ecd2b767751e5c41bd48d
Scala Steward <me@scala-steward.org>
2020-05-11 22:19:23 +0200

    Update akka-http, akka-http-spray-json, ... to 10.1.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   object Versions {
     lazy val Scala               = "2.12.10"
     lazy val Akka                = "2.6.4"
-    lazy val AkkaHttp            = "10.1.11"
+    lazy val AkkaHttp            = "10.1.12"
     lazy val Alpakka             = "2.0.2"
     lazy val ConfluentPlatform   = "5.4.0"
     lazy val JakartaWsRs         = "2.1.6"
