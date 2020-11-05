8e92a023e30c65586020e6cb8352fdaa055e6209
Scala Steward <me@scala-steward.org>
2020-11-05 20:14:06 +0100

    Update embedded-kafka-schema-registry to 5.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     lazy val AkkaHttp            = "10.1.12"
     lazy val Alpakka             = "2.0.5"
     lazy val ConfluentPlatform   = "5.4.3"
-    lazy val EmbeddedKafka       = "5.4.1.2"
+    lazy val EmbeddedKafka       = "5.5.1"
     lazy val JakartaWsRs         = "2.1.6"
     lazy val Kafka               = "2.4.1"
     lazy val KafkaCommons        = "0.0.8"
