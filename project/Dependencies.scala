ecfe6486c84ebf29e70fa32c60d6a10484fb5a4f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-12-31 14:59:37 +0100

    Update jakarta.ws.rs-api to 3.0.0 (#163)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     lazy val Alpakka             = "2.0.5"
     lazy val ConfluentPlatform   = "5.4.3"
     lazy val EmbeddedKafka       = "5.4.1.2"
-    lazy val JakartaWsRs         = "2.1.6"
+    lazy val JakartaWsRs         = "3.0.0"
     lazy val Kafka               = "2.4.1"
     lazy val KafkaCommons        = "0.0.8"
     lazy val KafkaDVS            = "1.0.18"
