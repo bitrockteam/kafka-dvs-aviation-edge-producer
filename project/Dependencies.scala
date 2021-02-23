86f63552f412694f7a305bb2f86ec48bb5217543
Scala Steward <me@scala-steward.org>
2021-02-23 18:15:22 +0100

    Update akka-stream, akka-stream-testkit to 2.6.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
   object Versions {
     lazy val Scala               = "2.12.10"
-    lazy val Akka                = "2.6.10"
+    lazy val Akka                = "2.6.13"
     lazy val AkkaHttp            = "10.2.2"
     lazy val Alpakka             = "2.0.6"
     lazy val ConfluentPlatform   = "5.4.3"
